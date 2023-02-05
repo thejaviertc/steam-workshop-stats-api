034ff5e1358649e90d5a99c6905c59a978273b01
thejaviertc <javier.toribiocouz@gmail.com>
2023-02-05 15:38:35 +0100

    Fix infinite loop with tests that have setInterval inside

--- src/tests/IpUtils.test.ts
+++ src/tests/IpUtils.test.ts
@@ -1,16 +1,20 @@
 import IpUtils from "../utils/IpUtils.js";
+jest.useFakeTimers();
+jest.spyOn(global, "setTimeout");
 
 describe("SteamUser", () => {
-	const ipUtils = new IpUtils();
-
 	test("Returns 'false' when the IP appears for the first time", () => {
+		const ipUtils = new IpUtils();
+
 		expect(
 			ipUtils.hasReachedLimit("192.168.1.30")
 		).toBeFalsy();
 	});
 
 	test("Returns 'true' when the IP appears reaches the limit", () => {
-		for (let i = 0; i < 98; i++)
+		const ipUtils = new IpUtils();
+
+		for (let i = 0; i < 99; i++)
 			ipUtils.hasReachedLimit("192.168.1.30");
 
 		expect(
