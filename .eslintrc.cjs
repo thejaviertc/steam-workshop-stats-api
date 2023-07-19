63882b704c65ae75f5457cfcc55f2dc971e70043
thejaviertc <javier.toribiocouz@gmail.com>
2023-07-19 19:27:02 +0200

    Actualiza dependencias

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,9 +3,14 @@ module.exports = {
 		browser: true,
 		es2021: true,
 	},
-	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+	extends: [
+		"plugin:@typescript-eslint/recommended-type-checked",
+		"plugin:@typescript-eslint/stylistic-type-checked",
+		"prettier",
+	],
 	parser: "@typescript-eslint/parser",
 	parserOptions: {
+		project: true,
 		ecmaVersion: "latest",
 		sourceType: "module",
 	},
